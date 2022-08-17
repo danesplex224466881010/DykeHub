741e207a5f288d9dada2e7178238c241fef62309
danesplex224466881010 <111463141+danesplex224466881010@users.noreply.github.com>
2022-08-17 17:06:57 -0400

    Update Main.lua

---
+++ Main.lua
@@ -0,0 +1,17 @@
+hookfunction(game:GetService("LogService").GetLogHistory, function()  return {} end)
+for i,v in next, getconnections(game:GetService("ScriptContext").Error) do
+	v:Disable()
+end
+repeat task.wait() until game:IsLoaded();
+_G.start = os.clock();
+
+local ids = {
+    ["111111111"] = "https://raw.githubusercontent.com/danesplex224466881010/DykeHub/game"
+}
+
+
+for i,v in pairs(ids) do
+    if tostring(game.GameId) == i or tostring(game.PlaceId) == i then
+        loadstring(game:HttpGet(v))();
+    end
+end
