1aa7ee36fa3c426a1190195759fd90928918e88f
danesplex224466881010 <111463141+danesplex224466881010@users.noreply.github.com>
2022-10-28 18:05:05 -0400

    Update Clicker Simulator.lua

--- Clicker Simulator.lua
+++ Clicker Simulator.lua
@@ -9,15 +9,6 @@ if _G.KeyEntered == true then
 		game:GetService("VirtualUser"):Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
 	end)
 
-	local EggTable = {}
-	local ZoneTable = {}
-	for _,Egg in pairs(game:GetService("Workspace").Eggs:GetChildren()) do
-    	table.insert(EggTable, Egg.Name)
-	end
-	for _,Zone in pairs(game:GetService("Workspace").Zones:GetChildren()) do
-    	table.insert(ZoneTable, Zone.Name)
-	end
-
 	local Window = OrionLib:MakeWindow({Name = "Dyke Hub |"..Player.UserId.."メ", HidePremium = true, IntroEnabled = false})
 
 	OrionLib:MakeNotification({
@@ -29,36 +20,9 @@ if _G.KeyEntered == true then
 
 	_G.Input = "string"
 	
-	local CharTab = Window:MakeTab({
-		Name = "Character",
-		Icon = "rbxassetid://4483345998",
-		PremiumOnly = false
-	})
-
-	local MainTab = Window:MakeTab({
-		Name = "Main",
-		Icon = "rbxassetid://4483345998",
-		PremiumOnly = false
-	})
-
-	MainTab:AddToggle({
-		Name = "Autoclick",
-		Default = false,
-		Callback = function(Value)
-			autoclick = Value
-		end    
-	})
+	
 
 	OrionLib:Init()
-
-	spawn(function()
-		while true do wait()
-			if autoclick then
-				getsenv(game:GetService("Players").LocalPlayer.PlayerGui.mainUI.LocalScript).activateClick()
-			end
-		end
-	end)
-
 else
 	game.Players.LocalPlayer:Kick("wahhh")
 end
