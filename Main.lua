3f753db85eca6822a1cdcede79bb1f6c341fbad8
danesplex224466881010 <111463141+danesplex224466881010@users.noreply.github.com>
2022-10-28 18:02:27 -0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -14,6 +14,7 @@ for i,v in pairs(ids) do
     if tostring(game.GameId) == i or tostring(game.PlaceId) == i then
         loadstring(game:HttpGet(v))();
     else
-        
+        game.Players.LocalPlayer:Kick("This game is not supported for Dyke Hub. The link to our discord has been copied, if you would like to join.")
+	setclipboard("https://discord.gg/ks4d3ZHT9Q")
     end
 end
